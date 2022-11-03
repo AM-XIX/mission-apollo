38ed5d89653e8b2be85af95e1eff0f2848cc4dff
Louise <louise.peridy22@gmail.com>
2022-11-03 09:14:06 +0100

    ajout app

--- src/index.js
+++ src/index.js
@@ -109,11 +109,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: 'https://flyby-gateway.herokuapp.com/',
+  uri: 'http://localhost:4000/',
   cache: new InMemoryCache(),
 });
 
-// Supported in React 18+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
